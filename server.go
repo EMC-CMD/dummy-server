9e545798a88270f652ad0a6049c662ab03adc0fe
Scott Weiss <scott.weiss@emc.com>
2015-10-16 15:33:23 -0400

    add simple file upload/download
    Signed-off-by: Scott Weiss <scott.weiss@emc.com>

--- server.go
+++ server.go
@@ -6,14 +6,28 @@ import (
 	"io/ioutil"
 	"encoding/json"
 	"fmt"
+	"os"
 )
 
 type inBody struct {
 	In	string	`json:"in"`
 }
 
+type Docker struct {
+	Name string `json:"Name"`
+	Image string `json:"Image"`
+	Command string `json:"Command"`
+}
+
+type Tarball struct {
+	Data []byte `json:"Data"`
+	Container Docker `json:"Container"`
+}
+
 func main() {
 	var inputCollection []string
+
+	var uploads map[string]
 
 	m := martini.Classic()
 	m.Get("/", func() string {
@@ -23,6 +37,7 @@ func main() {
 		}
 		return collection
 	})
+
 	m.Post("/in", func(res http.ResponseWriter, req *http.Request) string {
 		body, err := ioutil.ReadAll(req.Body)
 		if err != nil {
@@ -36,5 +51,56 @@ func main() {
 		inputCollection = append(inputCollection, in.In)
 		return fmt.Sprintf("here's what you gave me: " + in.In + "\ncurrent collection size: %v", len(inputCollection))
 	})
+	m.Post("/upload_container", func(res http.ResponseWriter, req *http.Request) string {
+		body, err := ioutil.ReadAll(req.Body)
+		if err != nil {
+			return "something went wrong"
+		}
+		var tarball Tarball
+		err = json.Unmarshal(body, &tarball)
+		if err != nil {
+			return "data did not read as Container/Data pair" + string(body)
+		}
+//		path := "./uploads/"+tarball.Container.Name+".tgz"
+//		if _, err := os.Stat(path); !os.IsNotExist(err) {
+//			return "this container has already been uploaded"
+//		}
+//		err = writeFileToDisk(path, tarball.Data)
+//		if err != nil {
+//			return "error writing file to disk" + err.Error()
+//		}
+		if _, haskey := uploads[tarball.Container.Name]; haskey {
+			return "error: "+tarball.Container.Name+" has already been uploaded."
+		}
+		uploads[tarball.Container.Name] = tarball
+		return tarball.Container.Name + " saved successfully"
+	})
+
+	m.Post("/download_container/:container_name", func(params martini.Params) []byte {
+		containerName := params["container_name"]
+		if _, haskey := uploads[containerName]; haskey {
+			return "error: "+containerName+" does not exist."
+		}
+		tarball := uploads[containerName]
+		response, err := json.Marshal(tarball)
+		if err != nil {
+			return "error converting tarball into a response"
+		}
+		delete(uploads, containerName)
+		return response
+	})
+
+
+
 	m.Run()
 }
+
+//func writeFileToDisk(path string, data []byte) error {
+//	err := ioutil.WriteFile(path, data, 0666)
+//	return err
+//}
+//
+//func readFileFromDisk(path string) ([]byte, error) {
+//	data, err := ioutil.ReadFile(path)
+//	return data, err
+//}
